a553a0261d589f5fb63bc1ee91ea45ac912918c0
kazuki <k016c1308@it-neec.jp>
2019-12-21 22:22:21 +0900

    Initial Commit

---
+++ AugMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AugMap
+//
+//  Created by kaguya on 2019/12/21.
+//  Copyright © 2019 kaguya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
