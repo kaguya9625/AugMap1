c5af913cb87c38d365fca73f086fb0269fd3c52a
kagu <k016c1308@it-neec.jp>
2020-01-08 11:50:40 +0900

    a

--- AugMap/ARViewController.swift
+++ AugMap/ARViewController.swift
@@ -24,19 +24,20 @@ class ARViewController: UIViewController,
     var pinlat:Float = 0.0
     var currentlon:Float = 0.0
     var currentlat:Float = 0.0
-    
+    var anchor:AnchorEntity!
      
     var distance:Double = 0
     var globaldz:Float = 0.0
     var globaldx:Float = 0.0
     
     @IBAction func backbutton(_ sender: Any) {
-           //遷移処理
-           let storyboard: UIStoryboard = self.storyboard!
+        //遷移処理
+          let storyboard: UIStoryboard = self.storyboard!
            let mappage = storyboard.instantiateViewController(withIdentifier: "mappage")
            self.present(mappage,animated: true,completion: nil)
+        arView.scene.removeAnchor(anchor)
        }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLocationManager()
@@ -61,12 +62,14 @@ class ARViewController: UIViewController,
     
     func setobj(){
         let Aug = CalcAngle(pinlat, currentlat, pinlon, currentlon)
-        let anchor1 = AnchorEntity(world: Aug)
-        arView.scene.anchors.append(anchor1)
+        anchor = AnchorEntity(world: Aug)
+        arView.scene.anchors.append(anchor)
         guard let model = try? Entity.load(named:"art.scnassets/tinko") else {return}
-        let unko = SIMD3<Float>(30,30,30)
+        let _check = check()
+        let unko = SIMD3<Float>(Float(_check),Float(_check),Float(_check))
         model.scale = unko
-        anchor1.addChild(model)
+        anchor.addChild(model)
+        
     }
     //オブジェクト座標算出メソッド
     func CalcAngle(_ pinlat1:Float ,_ currentlat2:Float,_ pinlon1:Float,_ currentlon2:Float) -> SIMD3<Float>{
@@ -80,10 +83,13 @@ class ARViewController: UIViewController,
         case 1:
             globaldx = globaldx/4
             globaldz = globaldz/4
+        case 2:
+            globaldx = globaldx/2
+            globaldz = globaldz/2
         default:
             print("default")
         }
-        let data = SIMD3<Float>(globaldx,-1,globaldz)
+        let data = SIMD3<Float>(globaldx,-1.25,globaldz)
         return data
     }
     
@@ -94,6 +100,7 @@ class ARViewController: UIViewController,
         let Cdistance = pin.distance(from: now)
         return Cdistance
     }
+    
     func judg() ->Int{
         var data = 0
         let Current_lat = Float((locManager.location?.coordinate.latitude)!)
@@ -103,8 +110,12 @@ class ARViewController: UIViewController,
         if distance <= 3000 && 1000 <= distance{
             print("4/1")
             data = 1
+        }else if distance <= 1001 && 750 <= distance{
+            print("2/1")
+            data = 2
+ 
         }else{
-            print("non")
+           print("non")
         }
         return data
     }
@@ -112,21 +123,31 @@ class ARViewController: UIViewController,
     func check()->Int{
         var unko = 0
         if distance <= 3000 && distance >= 2501{
-            unko = 80
-        }else if distance <= 2500 && distance >= 2001{
-            unko = 70
-        }else if distance <= 2000 && distance >= 1501{
             unko = 60
-        }else if distance <= 1500 && distance >= 1001{
+        }else if distance <= 2500 && distance >= 2001{
+            unko = 55
+        }else if distance <= 2000 && distance >= 1501{
             unko = 50
+        }else if distance <= 1500 && distance >= 1001{
+            unko = 45
         }else if distance <= 1000 && distance >= 501{
             unko = 40
-        }else if distance <= 500 && distance >= 101{
+        }else if distance <= 500 && distance >= 401{
+            unko = 35
+        }else if distance <= 400 && distance >= 301{
             unko = 30
+        }else if distance <= 300 && distance >= 201{
+            unko = 20
+        }else if distance <= 200 && distance >= 101{
+            unko = 15
         }else if distance <= 100 && distance >= 51{
-            unko = 10
+            unko = 8
+        }else if distance <= 50 && distance >= 31{
+            unko = 5
+        }else if distance <= 30 && distance >= 0{
+            unko = 1
         }
-        
+        print(unko)
         return unko
     }
 }
